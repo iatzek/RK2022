4e30d32cf66a525d3bbc3b090315a838cfbda13c
Jacek Kawa <jacek.kawa@polsl.pl>
2022-03-30 09:29:32 +0200

    feature: rozpisz przypadek x deltą = 0 i >0

--- RK2022/Program.cs
+++ RK2022/Program.cs
@@ -19,13 +19,18 @@ namespace RK2022
 
             double delta = b * b - 4 * a * c;
 
-            if (delta >= 0)
+            if (delta > 0)
             {
                 double x1 = (-b - Math.Sqrt(delta)) / 2 * a;
                 double x2 = (-b + Math.Sqrt(delta)) / 2 * a;
 
                 Console.WriteLine("x1={0} x2={1}", x1, x2);
-            } else
+            } 
+            else if (delta == 0) 
+            {
+                Console.WriteLine("x1=x2={0}", (-b) / (2 * a));
+            } 
+            else
             {
                 Console.WriteLine("brak rozwiązań");
             }            
