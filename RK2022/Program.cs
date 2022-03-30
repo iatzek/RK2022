6a0cc12c34d1f9c7db7a12fa23c50ef587e742a4
Jacek Kawa <jacek.kawa@polsl.pl>
2022-03-30 09:18:16 +0200

    Pierwsza działająca wersja

--- RK2022/Program.cs
+++ RK2022/Program.cs
@@ -6,7 +6,17 @@ namespace RK2022
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // ax2 + bx + c = 0
+            double a = 1, b = 2, c = -10;
+
+            double delta = b * b - 4 * a * c;
+
+            double x1 = (-b - Math.Sqrt(delta)) / 2 * a;
+            double x2 = (-b + Math.Sqrt(delta)) / 2 * a;
+
+            Console.WriteLine("x1={0} x2={1}", x1, x2);
+
+
         }
     }
 }
